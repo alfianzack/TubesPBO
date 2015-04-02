24205a21b1c533e7086371dd96ea282949c1c804
Eriza Tri Abdi N <eriza_nugroho@ymail.com>
2015-04-02 21:04:21 +0700

    Ambil nilai

--- Mapel.java
+++ Mapel.java
@@ -1,15 +1,21 @@
 public class Mapel {
-	private String namaMapel;
-	public void setNilai(String namaMapel){
-		this.namaMapel= namaMapel;
-	}
+    private String namaMapel;
+    private Guru g; //Buat nanti ngambil nilai
+    
+    public void setNilai(String namaMapel) {
+        this.namaMapel = namaMapel;
+    }
 
-	public String getNMapel{
-		return namaMapel;
-	}
+    public String getNamaMapel() {
+        return namaMapel;
+    }
 
-	public String toString{
-		return "Mapel : "+getNMapel()+" Nilai : "+getNilai();
-	}
+    public void setNamaMapel(String namaMapel) {
+        this.namaMapel = namaMapel;
+    }
+
+    public String toString() {
+        return "Mapel : " + getNamaMapel() + " Nilai : " + g.getNilai();
+    }
 
 }
