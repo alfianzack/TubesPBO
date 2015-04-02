21cf704b809558884bb33da4f38e108f30023871
Eriza Tri Abdi N <eriza_nugroho@ymail.com>
2015-04-02 20:57:07 +0700

    Input nilai harunya di guru bukan di siswa

--- Mapel.java
+++ Mapel.java
@@ -1,18 +1,5 @@
-public class Mapel implements Nilai{
+public class Mapel {
 	private String namaMapel;
-	private int uas,uts,kuis,tugas;
-
-	public void setNilai(int tugas,int uas,int uts,int kuis){
-		this.uas= uas;
-		this.uts= uts;
-		this.kuis= kuis;
-		this.tugas= tugas;
-	}
-
-	public int getNilai(){
-		return (tugas+uas+uts+kuis)/4;
-	}
-
 	public void setNilai(String namaMapel){
 		this.namaMapel= namaMapel;
 	}
