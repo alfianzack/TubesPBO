e017eac499d15acf4782e6bb378c97a6e9c48c95
Yodie Prasastya <prasastyad@gmail.com>
2015-04-03 21:52:43 +0700

    Update Mapel,interface,ViewtoString,hapus Nilai

--- Mapel.java
+++ Mapel.java
@@ -1,27 +1,82 @@
-public class Mapel {
+public class Mapel implements ViewtoString {
+
     private String namaMapel;
-    private Guru g; //Buat nanti ngambil nilai
-    
+    private int tugas, uts, uas, kuis;
+
+    /**
+     * Setter keempat nilai
+     *
+     * @param tugas
+     * @param uts
+     * @param uas
+     * @param kuis
+     */
+    public void setNilai(int tugas, int uts, int uas, int kuis) {
+        this.tugas = tugas;
+        this.uts = uts;
+        this.uas = uas;
+        this.kuis = kuis;
+    }
+
+    /**
+     * Get nama Mapel
+     *
+     * @return Nilai Tugas
+     */
+    public int getTugas() {
+        return tugas;
+    }
+
+    /**
+     * Get uts
+     *
+     * @return Nilai UTS
+     */
+    public int getUts() {
+        return uts;
+    }
+
+    /**
+     * Get UAS
+     *
+     * @return Nilai UAS
+     */
+    public int getUas() {
+        return uas;
+    }
+
+    /**
+     * Get Kuis
+     *
+     * @return Nilai Kuis
+     */
+    public int getKuis() {
+        return kuis;
+    }
+
     /**
      * Set nama Mapel
-     * @param namaMapel 
+     *
+     * @param namaMapel
      */
     public void setNamaMapel(String namaMapel) {
         this.namaMapel = namaMapel;
     }
-    
+
     /**
      * Get nama Mapel
+     *
      * @return namaMapel
      */
     public String getNamaMapel() {
         return namaMapel;
     }
-    
-    /**
-     * toString pada class Mapel
-     * @return Nama Mapel
-     */
+        /**
+         * toString pada class Mapel
+         *
+         * @return Nama Mapel
+         */
+    @Override
     public String toString() {
         return "\nMapel : " + getNamaMapel();
     }
