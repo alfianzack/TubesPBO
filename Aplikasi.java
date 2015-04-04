45d706428a7d1a26e4b468bea0c60de01fbfb74e
Eriza Tri Abdi N <eriza_nugroho@ymail.com>
2015-04-04 13:38:09 +0700

    Kebutuhan yg harus ada sudah di update
    Silahkan dibuat programnya

--- Aplikasi.java
+++ Aplikasi.java
@@ -18,31 +18,59 @@ public class Aplikasi {
 
     /**
      */
-    public static void menuSiswa() {
+    public static void mainMenu() {
+        System.out.println("0. EXIT");
+        System.out.println("1. Guru ");
+        System.out.println("2. Siswa ");
+        System.out.print("Pilih menu : ");
+    }
 
+    public static void menuSiswa() {
+        do {
+            System.out.println("MENU SISWA");
+            System.out.println("1. Tambah Mapel");
+            System.out.println("2. Lihat Nilai");
+            System.out.println("3. EXIT");
+            System.out.print("Pilih Menu : ");
+            pil = s.nextInt();
+            if (pil >= 4) {
+                System.out.println("Menu tidak ada, Ulangi lagi");
+            }
+        } while (pil != 3);
     }
 
     public static void menuGuru() {
-
+        do {
+            System.out.println("MENU GURU");
+            System.out.println("1. Input nilai");
+            System.out.println("2. Tambah siswa ke dalam kelas");
+            System.out.println("3. Daftar siswa");
+            System.out.println("4. Daftar nilai siswa");
+            System.out.println("5. EXIT");
+            System.out.print("Pilih Menu : ");
+            pil = s.nextInt();
+            if (pil > 5) {
+                System.out.println("Menu tidak ada, Ulangi lagi");
+            }
+        } while (pil != 5);
     }
 
     public static int inputPilihan() {
         try {
             pil = s.nextInt();
-            if ((pil >= 1) && (pil <= 2)) {
-                switch (pil) {
-                    case 1:
-                        menuGuru();
-                    case 2:
-                        menuSiswa();
-                }
-            } else if (pil > 2) {
-                throw new RuntimeException();
+            switch (pil) {
+                case 1:
+                    menuGuru();
+                    break;
+                case 2:
+                    menuSiswa();
+                    break;
+            }
+            if (pil > 2) {
+                System.out.println("Menu tidak ada, Ulangi lagi");
             }
         } catch (InputMismatchException e) {
             System.out.println("Input salah");
-        } catch (RuntimeException e) {
-            System.out.println("Pilih 0 - 2, Ulangi lagi");
         } finally {
             s = new Scanner(System.in);
         }
@@ -54,15 +82,23 @@ public class Aplikasi {
 //        Siswa s2 = new Siswa("Tset2", "921993");
 //        Kelas k1 = new Kelas();
 //
+//        Mapel m1 = new Mapel("Fisika");
+//        Mapel m2 = new Mapel("Kalkulus");
+//        Mapel m3 = new Mapel("Sejarah");
 //        k1.addSiswa(s1);
 //        k1.addSiswa(s2);
-//        System.out.println(k1.getJumlahSiswa());
+//        s1.ambilMapel(m1);
+//        s1.ambilMapel(m2);
+//        s1.ambilMapel(m3);
+//        s2.ambilMapel(m2);
+//        k1.viewSiswa();
+//        s1.viewMapel();
+        Sekolah sklh = new Sekolah();
+        sklh.setNamaSekolah("SMA Negeri Bandung");
         try {
             do {
-                System.out.println("0. EXIT");
-                System.out.println("1. Guru ");
-                System.out.println("2. Siswa ");
-                System.out.print("Pilih menu : ");
+                System.out.println(sklh.getNamaSekolah());
+                mainMenu();
                 inputPilihan();
             } while (pil != 0);
             System.out.println("Terima Kasih");
